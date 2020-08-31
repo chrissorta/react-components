c2270b72948fd13ff6fdc1b36159613edf6d9a36
Christopher Sorta <chris.sorta@yahoo.com>
2020-08-31 11:39:16 -0700

    Implement GroceryList component with dynamic array

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 // TODO
+
+
+var GroceryList = (props) => (
+  <div>
+    <ul>{
+      props.item.map((item, i) =>
+      <GroceryListItem  key={i} item = {item} />
+      )}
+    </ul>
+  </div>
+
+);
+
+var GroceryListItem = (props) => (
+<li>{props.item}</li>
+
+);
+
+var Bananas = () => (
+  <li>Bananas</li>
+);
+
+var Oranges = () => (
+  <li>Oranges</li>
+);
+
+ReactDOM.render(<GroceryList item = {['Banana', 'Orange', 'Flour']} />, document.getElementById("app"));
