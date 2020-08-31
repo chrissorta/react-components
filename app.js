73a102acf022a5b0f95ab7ccaae593884a9dd302
Christopher Sorta <chris.sorta@yahoo.com>
2020-08-31 12:40:56 -0700

    Implment event handler for hover over list item

--- app.js
+++ app.js
@@ -1,28 +1,67 @@
 // TODO
 
 
-var GroceryList = (props) => (
-  <div>
-    <ul>{
-      props.item.map((item, i) =>
-      <GroceryListItem  key={i} item = {item} />
-      )}
-    </ul>
-  </div>
+class GroceryList extends React.Component{
 
-);
+  constructor(props) {
+    super(props);
 
-var GroceryListItem = (props) => (
-<li>{props.item}</li>
+    // this.state = {
+    //   hovered: false
+    // }
+  }
 
-);
+  // onListItemHover() {
+  //   this.setState({
+  //     hovered: this.state.hovered!
+  //   })
+  // }
 
-var Bananas = () => (
-  <li>Bananas</li>
-);
 
-var Oranges = () => (
-  <li>Oranges</li>
-);
+  render(){
+
+
+    return (<div>
+      <ul>{
+        this.props.item.map((item, i) =>
+        <GroceryListItem  key={i} item = {item} />
+        )}
+      </ul>
+    </div>);
+  }
+
+
+
+}
+
+
+
+class GroceryListItem extends React.Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hovered: false
+    }
+  }
+
+  onListItemHover() {
+    this.setState({
+      hovered: !this.state.hovered
+    })
+  }
+
+  render(){
+    var style = {
+      "font-weight": this.state.hovered ? 'bold' : 'normal'
+    };
+
+    return  (
+      <li style={style} onMouseEnter={this.onListItemHover.bind(this)} onMouseLeave={this.onListItemHover.bind(this)}>{this.props.item}</li>
+    );
+  }
+
+
+}
 
 ReactDOM.render(<GroceryList item = {['Banana', 'Orange', 'Flour']} />, document.getElementById("app"));
